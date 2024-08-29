ee5f787d9786e863d9a5abc4fdb50232411d7642
Krisnaaassss <krisnalucianosiburian29@students.unnes.ac.id>
2024-08-29 10:31:05 +0700

    make order model and model function

--- Backend/controllers/orderController.js
+++ Backend/controllers/orderController.js
@@ -1,7 +1,48 @@
 import asyncHandler from "../middlewares/asyncHandler.js";
+import Product from "../models/productModel.js";
+import Order from "../models/orderModel.js";
 
 export const createOrder = asyncHandler(async (req, res) => {
+  const { email, firstName, lastName, phone, itemsDetail, cartItem } = req.body;
+
+  if (!cartItem || cartItem.length < 0) {
+    res.status(400);
+    throw new Error("Cart is empty");
+  }
+
+  let orderItem = [];
+  let total = 0;
+
+  for (const cart of cartItem) {
+    const productData = await Product.findOne({ _id: cart.product });
+    if (!productData) {
+      res.status(404);
+      throw new Error("ID Product Not Found");
+    }
+    const { name, price, _id } = productData;
+    const singleProduct = {
+      quantity: cart.quantity,
+      name,
+      price,
+      product: _id,
+    };
+    total += cart.quantity * price;
+    orderItem = [...orderItem, singleProduct];
+  }
+
+  const order = await Order.create({
+    email,
+    firstName,
+    lastName,
+    phone,
+    itemsDetail: orderItem,
+    total,
+    user: req.user._id,
+  });
+
   res.status(201).json({
+    total,
+    order,
     message: "Order Created",
   });
 });
